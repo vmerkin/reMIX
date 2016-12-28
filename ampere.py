73e763e3f5ec339d4cc32fa42ae13cdf37c690e2
vmerkin <slava.merkin@jhuapl.edu>
2016-12-28 14:39:34 -0500

    Adding area calculation for the AMPERE grid.

--- ampere.py
+++ ampere.py
@@ -1,4 +1,4 @@
-from numpy import loadtxt,pi
+from numpy import loadtxt,pi,sin
 
 class ampere():
     def __init__(self,file_name):
@@ -8,7 +8,18 @@ class ampere():
     def get_grid(self):
         p = self.data[:24,:]*pi/12.
         t = self.data[24:48,:]*pi/180.
-        return(t.T,p.T)  # note, always have theta first, phi second
+        
+        # FIXME: hardwiring uniform spherical grid for now
+        # need to generalized for arbirary grids
+        dt = t[0,1]-t[0,0]
+        dp = p[1,0]-p[0,0]
+        A = sin(t)*dt*dp
+
+        # note, always have theta first, phi second
+        # return area as well
+        return(t.T,p.T,A.T)  
+
 
     def get_data(self):
         return self.data[48:,:].T
+
