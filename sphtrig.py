ec8cf63f5f54b051f73692bb10cd3e30765c70bc
vmerkin <slava.merkin@jhuapl.edu>
2016-12-28 14:49:27 -0500

    Just renaming some stuff.(?)

--- sphtrig.py
+++ sphtrig.py
@@ -1,15 +1,23 @@
-from numpy import arccos,tan,sin
+from numpy import arccos,tan,sin,cos
 import point
 import util
 
 # theta prime angle
-def t_p(ps,po):
-	return arccos(util.dot(ps.r_u(),po.r_u()))
+def theta_p(ps,pd):
+        # this asserts that our calculation of theta_p agrees with
+        # Vanhamaki, eq A3 assertion on floats is a bad idea anyway,
+        # but I checked that the two calculations agree to the last
+        # significant digit
+        #
+        # assert(cos(pd.t)*cos(ps.t)+sin(pd.t)*sin(ps.t)*cos(ps.p-pd.p)),
+        # util.dot(ps.r_unit(),pd.r_unit())
+
+	return arccos(util.dot(ps.r_unit(),pd.r_unit()))
 
 # theta prime unit vector
-def t_p_u(ps,po):
-	return po.r_u()/tan(t_p(ps,po))-ps.r_u()/sin(t_p(ps,po))
+def theta_p_unit(ps,pd):
+	return pd.r_unit()/tan(theta_p(ps,pd))-ps.r_unit()/sin(theta_p(ps,pd))
 
 # phi prime unit vector
-def p_p_u(ps,po):
-	return util.cross(ps.r_u(),po.r_u())/sin(t_p(ps,po))
+def phi_p_unit(ps,pd):
+	return util.cross(ps.r_unit(),pd.r_unit())/sin(theta_p(ps,pd))
