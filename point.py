932040a8fffc5fd279f288db3ecfa602f1bfd430
vmerkin <slava.merkin@jhuapl.edu>
2016-12-28 14:47:49 -0500

    Fixing bug in assertion.

--- point.py
+++ point.py
@@ -4,10 +4,10 @@ class point():
 
     def __init__(self,t,p):
     	# assume both theta and phi are in radians
-    	assert t<=pi/2 and t>=0. and p<=pi and p>=0.
+    	assert t<=pi and t>=0. and p<=2.*pi and p>=0.
         self.t  = t
         self.p = p
 
   	# unit vector
-    def r_u(self):
+    def r_unit(self):
     	return(array([sin(self.t)*cos(self.p),sin(self.t)*sin(self.p),cos(self.t)]))
