3e99053cdd6bc6cf8e2ac56e52dc754321b0c54a
vmerkin <slava.merkin@jhuapl.edu>
2017-01-04 17:15:34 -0500

    Adding capability for interpolation to higher resolution grid.
    It is turned off for now because it is slow until we go to sparse matrices in the following commits. Also, removing explicit references to ampere grid in the variable names to simplify future generalizations.

--- poisson.py
+++ poisson.py
@@ -4,22 +4,37 @@ import numpy as np
 import sys
 import time
 import point,sphtrig
-from numpy import sin,cos,arccos,mat,dot,repeat,multiply,tan,pi,sqrt,append,linspace,meshgrid,zeros,arange
+from numpy import sin,cos,arccos,mat,dot,repeat,multiply,tan,pi,sqrt,append,linspace,meshgrid,zeros,arange,ones_like,array
 from matplotlib.pyplot import figure,subplot,pcolormesh,colorbar,contour,show,contourf,rgrids,ylim
 from scipy import interpolate
+import time
 
 if __name__ == "__main__":
-    Sigma = 8. # scalar for the time being
+    Sigma_const = 8. # scalar for the time being
+    interp = False
 
-    amp = ampere_pole.ampere_pole('data/AMPERE_north_2015-08-15T20-14-00Z.txt')
+    # AMPERE stuff
+    amp = ampere_pole.ampere_pole('data/AMPERE_north_2015-08-15T20-50-00Z.txt')
 
-    amp_grid = grid.generate_from_data(amp.get_grid())
-    amp_jr = amp.get_data()
+    grd = grid.generate_from_data(amp.get_grid())
+    jr = amp.get_data()
+    if interp:
+        ntheta = 100
+        nphi = 720
+        f = interpolate.RectBivariateSpline(grd.t[:,0],grd.p[0,:],jr,kx=1,ky=1)
+        new_t = linspace(grd.t[:,0].min(),grd.t[:,0].max(),ntheta)
+        new_p = linspace(grd.p[0,:].min(),grd.p[0,:].max(),nphi)
+        grd = grid.grid()
+        grd.t,grd.p = meshgrid(new_t,new_p,indexing='ij')
+        jr = f(grd.t[:,0],grd.p[0,:])
 
-    dt = amp_grid.t[1:,:]-amp_grid.t[:-1,:]
-    dp = amp_grid.p[:,1:]-amp_grid.p[:,:-1]
+    jr[abs(jr)<=0.2]=0.
 
-    (Nt,Np) = amp_grid.t.shape
+    dt = grd.t[1:,:]-grd.t[:-1,:]
+    dp = grd.p[:,1:]-grd.p[:,:-1]
+
+    (Nt,Np) = grd.t.shape
+    ############
 
     K = lambda i,j: j*Nt+i
     A=zeros((Nt*Np,Nt*Np))
@@ -37,15 +52,15 @@ if __name__ == "__main__":
             j      = j%(Np-1)
 
             ft = 2./(dt[i,j]+dt[i-1,j])
-            fp = 2./(dp[i,j]+dp[i,jm1])/sin(amp_grid.t[i,j])**2
+            fp = 2./(dp[i,j]+dp[i,jm1])/sin(grd.t[i,j])**2
 
-            A[K(i,jA),K(i,jA)] = -ft*(1/dt[i,j]+1/dt[i-1,j]) - fp*(1/dp[i,j]+1/dp[i,jm1])  - 0.5*ft*(dt[i,j]/dt[i-1,j]-dt[i-1,j]/dt[i,j])/tan(amp_grid.t[i,j])
-            A[K(i,jA),K(i+1,jA)] = ft*(1./dt[i,j]+0.5*dt[i-1,j]/dt[i,j]/tan(amp_grid.t[i,j]))
-            A[K(i,jA),K(i-1,jA)] = ft*(1./dt[i-1,j]-0.5*dt[i,j]/dt[i-1,j]/tan(amp_grid.t[i,j]))
+            A[K(i,jA),K(i,jA)] = -ft*(1/dt[i,j]+1/dt[i-1,j]) - fp*(1/dp[i,j]+1/dp[i,jm1])  - 0.5*ft*(dt[i,j]/dt[i-1,j]-dt[i-1,j]/dt[i,j])/tan(grd.t[i,j])
+            A[K(i,jA),K(i+1,jA)] = ft*(1./dt[i,j]+0.5*dt[i-1,j]/dt[i,j]/tan(grd.t[i,j]))
+            A[K(i,jA),K(i-1,jA)] = ft*(1./dt[i-1,j]-0.5*dt[i,j]/dt[i-1,j]/tan(grd.t[i,j]))
             A[K(i,jA),K(i,jAp1)] = fp/dp[i,j]
             A[K(i,jA),K(i,jAm1)] = fp/dp[i,jm1]
 
-            J[K(i,jA)] = amp_jr[i,j]
+            J[K(i,jA)] = jr[i,j]
 
     # low lat boundary
     for j in arange(0,Np):
@@ -63,7 +78,7 @@ if __name__ == "__main__":
     import scipy
     from scipy.sparse import linalg
     pot,status=scipy.sparse.linalg.lgmres(A,J)
-    pot*=6.5**2*1.e3/Sigma
+    pot*=6.5**2*1.e3/Sigma_const
 
     print "Convergence status: ", status
     print "Potential min/max (kV): ",pot.min(),pot.max()
@@ -76,7 +91,7 @@ if __name__ == "__main__":
     lbls = [str(elem)+u'\xb0' for elem in [10,20,30,40]] 
     lines, labels = rgrids(circles,lbls)
     ylim(0,44.*pi/180.)
-    contourf(amp_grid.p+pi+pi/2,amp_grid.t,pot.reshape(24,51).T,51)
+    contourf(grd.p+pi+pi/2,grd.t,pot.reshape(Np,Nt).T,51)
     colorbar().set_label('Potential, kV')
 
     figure() 
@@ -85,8 +100,8 @@ if __name__ == "__main__":
     lbls = [str(elem)+u'\xb0' for elem in [10,20,30,40]] 
     lines, labels = rgrids(circles,lbls)
     ylim(0,44.*pi/180.)
-    contourf(amp_grid.p+pi+pi/2,amp_grid.t,amp_jr,51)
+    contourf(grd.p+pi+pi/2,grd.t,jr,51)
     colorbar().set_label('Current, microA/m^2')
-    contour(amp_grid.p+pi+pi/2,amp_grid.t,pot.reshape(24,51).T,13,colors='black')
+    contour(grd.p+pi+pi/2,grd.t,pot.reshape(Np,Nt).T,13,colors='black')
     
 
