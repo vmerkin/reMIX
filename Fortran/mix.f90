d6c5a74a310c44a42fcfb850c74501d2adb10d6d
vmerkin <slava.merkin@jhuapl.edu>
2017-01-17 07:50:55 -0700

    Initial commit of the Fortran code.
    Way to go.

---
+++ Fortran/mix.f90
@@ -0,0 +1,55 @@
+include 'mkl_pardiso.f90'
+
+program MIX
+  use mkl_pardiso
+  
+  use mixdefs
+  use mixio
+  use mixtypes
+  
+  implicit none
+
+  character(len=strLen) :: fname
+  integer, dimension(6) :: simtime
+  integer, dimension(2) :: dims
+  real(mix_io_real), dimension(:,:), allocatable :: x_in,y_in,pot_in,j_in,sigmap_in,sigmah_in
+  type(Grid_T) :: G0
+  type(State_T) :: S0
+
+  call h5open_f(herror) !Setup H5 Fortran interface
+
+  fname = "Aug2010_mix_2010-08-04T00-00-00Z.h5"
+
+  ! FIXME: pack everything into one 3D array eventually; also define grid class
+  call getUT(fname,simtime)
+  call getVar(fname,"Grid X",x_in)
+  call getVar(fname,"Grid Y",y_in)
+  call getVar(fname,"Potential North [kV]",pot_in)
+  call getVar(fname,"FAC North [microAm2]",j_in)
+  call getVar(fname,"Pedersen conductance North [S]",sigmap_in)
+  call getVar(fname,"Hall conductance North [S]",sigmah_in)
+
+  dims = shape(x_in); G0%Nt = dims(1); G0%Np = dims(2)
+  allocate(G0%x(G0%Nt,G0%Np))
+  allocate(G0%y(G0%Nt,G0%Np))
+  allocate(G0%t(G0%Nt,G0%Np))
+  allocate(G0%p(G0%Nt,G0%Np))
+  allocate(S0%V(G0%Nt,G0%Np,nVars))
+
+  ! cast the arrays into whatever was defined as mix_real type (double by default)
+  ! grid things
+  G0%x = x_in
+  G0%y = y_in
+  ! define spherical angular coordinates
+  G0%t = asin(sqrt(G0%x**2+G0%y**2))
+  G0%p = mod((atan2(G0%y,G0%x)+2*pi),(2.*pi)) ! note, this mangles phi at theta=0, 
+  !but we don't care because the coordinates of that point are never used
+
+  S0%V(:,:,POT) = pot_in
+  S0%V(:,:,FAC) = j_in
+  S0%V(:,:,SIGMAP) = sigmap_in
+  S0%V(:,:,SIGMAH) = sigmah_in
+
+  write(*,*) S0%V
+  call h5close_f(herror)  ! Close H5 Fortran interface
+end program MIX
---
+++ Fortran/mixio.f90
@@ -0,0 +1,79 @@
+! Input/output for mix
+
+module mixio
+  use hdf5
+  use mixdefs  
+
+  implicit none
+
+  !Necessary for HDF5 routines
+  integer :: herror
+
+  contains
+
+    ! get UT: int array (Year, Month, Day, Hr, Min, Sec)
+    subroutine getUT(fname,simtime)
+      character(len=*),intent(in) :: fname
+      integer, dimension(6), intent(out) :: simtime
+      
+      integer(HID_T) :: h5fId,attrId
+
+      TYPE(C_PTR) :: f_ptr
+
+      call checkFile(fname)
+      !Open file
+      call h5fopen_f(fname, H5F_ACC_RDONLY_F, h5fId, herror)
+
+      call h5aopen_f(h5fId,"SimTime",attrId,herror)
+      f_ptr = C_LOC(simtime)
+      call h5aread_f(attrId,H5T_NATIVE_INTEGER,f_ptr,herror)
+      
+      call h5aclose_f(attrId,herror)
+      call h5fclose_f(h5fId, herror)
+    end subroutine getUT
+
+    subroutine getVar(fname,varname,var)
+      character(len=*),intent(in) :: fname
+      character(len=*),intent(in) :: varname
+      
+      integer(HID_T) :: h5fId,dsId,dspaceId
+      integer(HSIZE_T), dimension(2) :: dims,maxdims
+
+      TYPE(C_PTR) :: f_ptr
+
+      real, dimension(:,:), allocatable :: var
+!      real, dimension(27,180) :: var
+
+      call checkFile(fname)
+      !Open file
+      call h5fopen_f(fname, H5F_ACC_RDONLY_F, h5fId, herror)
+
+      call h5dopen_f(h5fId,varname,dsId,herror)
+      call h5dget_space_f(dsId,dspaceId,herror);
+      call h5sget_simple_extent_dims_f(dspaceId, dims, maxdims, herror) 
+
+      if (.not. allocated(var)) then 
+         allocate(var(1:dims(1),1:dims(2)))
+      end if
+
+      f_ptr = C_LOC(var)
+      call h5dread_f(dsId,H5T_NATIVE_REAL,f_ptr,herror)
+      
+      call h5dclose_f(dsId,herror)
+      call h5fclose_f(h5fId, herror)
+    end subroutine getVar
+
+    
+    ! check H5 file existence
+    subroutine checkFile(fname)
+      character(len=*),intent(in) :: fname
+      logical :: fExist
+
+      inquire(file=fname,exist=fExist)
+      if (.not.(fExist)) then
+         write(*,"(a,a)") "Cannot read file ", fname
+         stop
+      endif
+    end subroutine checkFile
+      
+end module mixio
